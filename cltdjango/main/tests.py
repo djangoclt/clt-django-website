202bc71bd80fec96f908889e94eac65bdf046fef
Mike Barkas <mike@barkas.com>
2016-11-02 21:52:35 -0400

    Issue-30: Start unit tests for main app.
      This adds basic URL unit tests and will allow us to set up
      Travis CI.

--- cltdjango/main/tests.py
+++ cltdjango/main/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
 
-# Create your tests here.
+from . import views
+
+
+class MainUrlTest(TestCase):
+
+    def test_home_url(self):
+        url = resolve('/')
+
+        self.assertEqual(url.func, views.index)
+        self.assertEqual(url.url_name, 'index')
+
+        resp = self.client.post('/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_active_members_url(self):
+        url = resolve('/active_members/')
+
+        self.assertEqual(url.func, views.active_members)
+        self.assertEqual(url.url_name, 'active_members')
+
+        resp = self.client.post('/active_members/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_page_not_found(self):
+        resp = self.client.post('/does-not-exist/')
+        self.assertEqual(resp.status_code, 404)
