99d5a5a42b7ba665d508901f90793a837002aa2d
Mike Barkas <mike@barkas.com>
2016-11-07 18:28:51 -0500

    Issue-35: Update unit test for active members URL.
      The URL changed and the test needed to be updated.
      Closes Issue #35

--- cltdjango/main/tests.py
+++ cltdjango/main/tests.py
@@ -16,12 +16,12 @@ class MainUrlTest(TestCase):
         self.assertEqual(resp.status_code, 200)
 
     def test_active_members_url(self):
-        url = resolve('/active_members/')
+        url = resolve('/active-members/')
 
         self.assertEqual(url.func, views.active_members)
         self.assertEqual(url.url_name, 'active_members')
 
-        resp = self.client.post('/active_members/')
+        resp = self.client.post('/active-members/')
         self.assertEqual(resp.status_code, 200)
 
     def test_page_not_found(self):
