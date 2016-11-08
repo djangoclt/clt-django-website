3bcd24a74e48addbcd864bf6184708e2b4d072c2
Mike Barkas <mike@barkas.com>
2016-11-07 21:00:38 -0500

    Issue-39: Remove and change secret key for production server.

--- cltdjango/cltdjango/settings.py
+++ cltdjango/cltdjango/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gq-c1en)6)td#m3vjqq@9gly#sn=y1p1jg&(8-$qvvyi6jq69*'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
